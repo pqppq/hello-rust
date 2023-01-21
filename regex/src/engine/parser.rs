4491fb684d807d2fcec7b27daa7e7fd5bbfdfa07
pqppq <yutatsu0119@gmail.com>
2023-01-21 18:08:53 +0900

    test: add test for parser

--- regex/src/engine/parser.rs
+++ regex/src/engine/parser.rs
@@ -6,14 +6,14 @@ use std::{
 };
 
 /// AST type
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum AST {
-    Char(char), // one character
-    Plus(Box<AST>), // +
-    Star(Box<AST>), // *
-    Question(Box<AST>), // ?
+    Char(char),             // one character
+    Plus(Box<AST>),         // +
+    Star(Box<AST>),         // *
+    Question(Box<AST>),     // ?
     Or(Box<AST>, Box<AST>), // |
-    Seq(Vec<AST>), // sequence of regex
+    Seq(Vec<AST>),          // sequence of regex
 }
 
 enum PSQ {
@@ -26,9 +26,9 @@ enum PSQ {
 #[derive(Debug)]
 pub enum ParseError {
     InvalidEscape(usize, char), // invalid escape sequence
-    InvalidRightParen(usize), // missing )
-    NoPrev(usize), // no expression before + | * ?
-    NoRightParen, // missing (
+    InvalidRightParen(usize),   // missing )
+    NoPrev(usize),              // no expression before + | * ?
+    NoRightParen,               // missing (
     Empty,
 }
 
@@ -56,7 +56,11 @@ impl Display for ParseError {
 impl Error for ParseError {}
 
 /// transform + * ? into AST
-fn parse_plus_star_question(seq: &mut Vec<AST>, ast_type: PSQ, pos: usize) -> Result<(), ParseError> {
+fn parse_plus_star_question(
+    seq: &mut Vec<AST>,
+    ast_type: PSQ,
+    pos: usize,
+) -> Result<(), ParseError> {
     if let Some(prev) = seq.pop() {
         let ast = match ast_type {
             PSQ::Plus => AST::Plus(Box::new(prev)),
@@ -83,7 +87,7 @@ fn parse_escape(pos: usize, c: char) -> Result<AST, ParseError> {
 
 // transform seq_or into OR AST
 fn fold_or(mut seq_or: Vec<AST>) -> Option<AST> {
-    if seq_or.len() > 1{
+    if seq_or.len() > 1 {
         let mut ast = seq_or.pop().unwrap();
         seq_or.reverse();
         for s in seq_or {
@@ -103,19 +107,19 @@ pub fn parse(expr: &str) -> Result<AST, ParseError> {
         Escape,
     }
 
-    let mut seq =  Vec::new(); // context
+    let mut seq = Vec::new(); // context
     let mut seq_or = Vec::new(); // OR context
     let mut stack = Vec::new(); // context stack
     let mut state = ParseState::Char; // current state
 
-    for  (i, c) in expr.chars().enumerate() {
+    for (i, c) in expr.chars().enumerate() {
         match &state {
             ParseState::Char => {
                 match c {
-                    '+'  => parse_plus_star_question(&mut seq, PSQ::Plus, i)?,
-                    '*'  => parse_plus_star_question(&mut seq, PSQ::Star, i)?,
-                    '?'  => parse_plus_star_question(&mut seq, PSQ::Question, i)?,
-                    '('  => {
+                    '+' => parse_plus_star_question(&mut seq, PSQ::Plus, i)?,
+                    '*' => parse_plus_star_question(&mut seq, PSQ::Star, i)?,
+                    '?' => parse_plus_star_question(&mut seq, PSQ::Question, i)?,
+                    '(' => {
                         // push current context to stack
                         // clean up current context
                         // takeは引数の値を返して、その引数を型のデフォルト値にする
@@ -143,9 +147,9 @@ pub fn parse(expr: &str) -> Result<AST, ParseError> {
                     '|' => {
                         if seq.is_empty() {
                             // "||", "(|abc)", etc.
-                            return Err(ParseError::NoPrev(i))
+                            return Err(ParseError::NoPrev(i));
                         } else {
-                            let prev =take(&mut seq);
+                            let prev = take(&mut seq);
                             seq_or.push(AST::Seq(prev))
                         }
                     }
@@ -163,7 +167,7 @@ pub fn parse(expr: &str) -> Result<AST, ParseError> {
     }
 
     if !stack.is_empty() {
-        return Err(ParseError::NoRightParen)
+        return Err(ParseError::NoRightParen);
     }
 
     if !seq.is_empty() {
@@ -174,5 +178,48 @@ pub fn parse(expr: &str) -> Result<AST, ParseError> {
         Ok(ast)
     } else {
         Err(ParseError::Empty)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::engine::parser::{parse, AST};
+
+    #[test]
+    fn parse_test() {
+        // simple cases
+        let expr: &str = "ab";
+        let expect: AST = AST::Seq(vec![AST::Char('a'), AST::Char('b')]);
+        assert_eq!(expect, parse(expr).unwrap());
+
+        let expr: &str = "ab+";
+        let expect: AST = AST::Seq(vec![AST::Char('a'), AST::Plus(Box::new(AST::Char('b')))]);
+        assert_eq!(expect, parse(expr).unwrap());
+
+        let expr: &str = "ab*";
+        let expect: AST = AST::Seq(vec![AST::Char('a'), AST::Star(Box::new(AST::Char('b')))]);
+        assert_eq!(expect, parse(expr).unwrap());
+
+        let expr: &str = "ab?";
+        let expect: AST = AST::Seq(vec![
+            AST::Char('a'),
+            AST::Question(Box::new(AST::Char('b'))),
+        ]);
+        assert_eq!(expect, parse(expr).unwrap());
+
+        let expr: &str = "a|b";
+        let expect: AST = AST::Or(
+            Box::new(AST::Seq(vec![AST::Char('a')])),
+            Box::new(AST::Seq(vec![AST::Char('b')])),
+        );
+        assert_eq!(expect, parse(expr).unwrap());
+
+        // complex cases
+        let expr: &str = "(a*)*";
+        let expect: AST = AST::Seq(
+            vec![AST::Star(Box::new(AST::Seq(
+                        vec![AST::Star(Box::new(AST::Char('a')))]
+                    )))]);
+        assert_eq!(expect, parse(expr).unwrap());
     }
 }
